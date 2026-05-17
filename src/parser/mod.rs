318a69f507d61d4a389c59e9c575bfee3945ac1c
Jeffsky <jjeffcaii@outlook.com>
2026-05-17 17:56:04 +0800

    chore: fix workflow

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -13,26 +13,7 @@ mod tests {
         pretty_env_logger::try_init_timed().ok();
     }
 
-    static MDX: Lazy<Mdx> = Lazy::new(|| {
-        parse("/Users/jeffsky/.yadict/mdicts/英汉大词典（第二版）陆谷孙.mdx")
-            .expect("failed to read mdx")
-    });
-
-    #[test]
-    fn test_keys() -> anyhow::Result<()> {
-        init();
-        for record in MDX.items() {
-            let key = String::from_utf8_lossy(record.key());
-            if key.starts_with("cat") {
-                let v = record
-                    .value()
-                    .map(|it| String::from_utf8_lossy(it).to_string());
-                info!("{} -- {}", key, v.unwrap());
-            }
-        }
-
-        Ok(())
-    }
+    static MDX: Lazy<Mdx> = Lazy::new(|| parse("/tmp/example.mdx").expect("failed to read mdx"));
 
     #[test]
     fn test_query() -> anyhow::Result<()> {
